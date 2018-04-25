e92a962c84d95fb38d0bb5260827bcaaf7761079
Bas van Mil <basjurre@gmail.com>
2018-04-25 14:36:01 +0200

    api hello world toegevoegt ook met een t

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('HALLO ALLEMAAL, WAT FIJN DAT JE ER BENT. KARNAVAL TWEEDUIZENDACHTIEN MODDERFOKKER!')
 });
  
+app.get('/api/helloworld', function (req, res) {
+  res.status(200).json({
+    message: 'Hello World!!!!11!'
+  }).end()
+});
+ 
 app.listen(3000);
